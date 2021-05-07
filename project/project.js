466188b302fcea9e7b89f2a40215bb3f36b98bab
lovelymoon3 <64762889+lovelymoon3@users.noreply.github.com>
2021-05-07 21:18:01 +0930

    Add files via upload

--- project/project.js
+++ project/project.js
@@ -6,7 +6,6 @@ function log_in(){
     log_in.style.display = "block";
     main.style.display = "none";
     sign_up.style.display = "none";
-
 }
 
 //back button
@@ -28,11 +27,3 @@ function sign_up(){
     main.style.display = "none";
     log_in.style.display = "none";
 }
-
-//about us pop-up window
-// function about_us(){
-//     // var new_window = window.open("", 'newwindow', 'width=300,height=250');
-//     // new_window.document.write("<h1>About us</h1><p>Hello! We are CS group!</p>");
-//     var about_us = document.getElementById("about_us");
-//     about_us.style.display = "block";
-// }
