1d989ce0e7e86c67ad1f5e1facaafa0245110c70
lovelymoon3 <64762889+lovelymoon3@users.noreply.github.com>
2021-06-08 23:23:11 +0930

    Add files via upload

--- project/public/javascripts/login.js
+++ project/public/javascripts/login.js
@@ -10,7 +10,7 @@ function onFailure(error) {
 function renderButton() {
       gapi.signin2.render('my-signin2', {
         'scope': 'profile email',
-        'width': 240,
+        'width': 400,
         'height': 50,
         'longtitle': true,
         'theme': 'dark',
