36b77db1ed6af3d54490004d5e58b51fffcd999e
lovelymoon3 <64762889+lovelymoon3@users.noreply.github.com>
2021-06-09 15:19:04 +0930

    Add files via upload

--- project/public/javascripts/login.js
+++ project/public/javascripts/login.js
@@ -25,6 +25,8 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+
 }
 
 
