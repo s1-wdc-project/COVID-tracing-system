c83b26ac1a581e935f21dbc2b74e14c35905bb95
Hao447496200 <80316951+Hao447496200@users.noreply.github.com>
2021-06-13 02:19:06 +0800

    Add files via upload

---
+++ projectAAA/public/javascripts/project.js
@@ -0,0 +1,355 @@
+// log-in button
+function log_in(){
+    var main = document.getElementById("main");
+    var log_in = document.getElementById("log_in");
+    var sign_up = document.getElementById("sign_up");
+    log_in.style.display = "block";
+    main.style.display = "none";
+    sign_up.style.display = "none";
+}
+
+//back button
+function showMain(){
+    var main = document.getElementById("main");
+    var log_in = document.getElementById("log_in");
+    var sign_up = document.getElementById("sign_up");
+    main.style.display = "block";
+    log_in.style.display = "none";
+    sign_up.style.display = "none";
+
+}
+//sign_up button
+function sign_up(){
+    var main = document.getElementById("main");
+    var log_in = document.getElementById("log_in");
+    var sign_up = document.getElementById("sign_up");
+    sign_up.style.display = "block";
+    main.style.display = "none";
+    log_in.style.display = "none";
+}
+
+
+// ------------------------------------ log in ----------------------------------------
+function go_to_user_page(){
+  location.replace("user_page.html");
+}
+
+// // function go_to_venue_page(){
+// //   location.replace("user_page.html");
+// // }
+
+// function user_define(){
+//     if(Math.floor(id/10000) == 1){
+//         user = {'user_id' : id, 'password' : pass};
+//     }else if(Math.floor(id/10000) == 2){
+//         user = {'venue_id' : id, 'password' : pass};
+//     }else if(Math.floor(id/10000) == 3){
+//         user = {'official_id' : id, 'password' : pass};
+//     }
+// }
+
+function show_user(){
+        var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("user_header_show").innerHTML = this.responseText;
+        var  recent_user = this.responseText;
+          console.log(recent_user);
+        }
+    };
+    xhttp.open("GET","/users/take_user_id", false);
+    xhttp.send();
+}
+
+function LogIn(){
+    var id = document.getElementById('user_enter_id').value;
+    var pass = document.getElementById('user_enter_password').value;
+    var xmlhttp = new XMLHttpRequest();
+    var user = {'user_id' : id, 'password' : pass};
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+                    go_to_user_page();
+                    show_user();
+        }else if(this.readyState == 4 && this.status >= 400){
+                    LogIn_v();
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/users/login", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
+
+}
+//--------------------------- log in venue
+
+function LogIn_v(){
+    var id = document.getElementById('user_enter_id').value;
+    var pass = document.getElementById('user_enter_password').value;
+    var xmlhttp = new XMLHttpRequest();
+
+   var user = {'user_id' : id, 'password' : pass};
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+                    go_to_user_page();
+                    show_user();
+        }else if(this.readyState == 4 && this.status >= 400){
+             LogIn_o();
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/users/login_v", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
+
+}
+
+// =============================log in official
+
+function LogIn_o(){
+    var id = document.getElementById('user_enter_id').value;
+    var pass = document.getElementById('user_enter_password').value;
+    var xmlhttp = new XMLHttpRequest();
+    var user = {'user_id' : id, 'password' : pass};
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+                    go_to_user_page();
+                    show_user();
+        }else if(this.readyState == 4 && this.status >= 400){
+             alert("Login failed");
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/users/login_o", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
+
+}
+// ----------------------------------- log out -----------------------------------------
+function LogOut(){
+
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
+
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Log Out successfully! " + this.responseText);
+        }else if(this.readyState == 4 && this.status >= 400){
+             alert("LogOut failed");
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/users/logout", true);
+    xmlhttp.send();
+
+}
+
+
+
+//---------------------------------------user sign up----------------------------------
+function user_sign_up(){
+    let data = {
+        first_name:document.getElementById("firstName").value,
+        last_name: document.getElementById("lastName").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        phone: document.getElementById("phone").value
+    };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // app.get_post();
+        }
+    };
+    xhttp.open("POST","/user_sign_up", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(data));
+    console.log(data);
+}
+
+
+//---------------------------venue-sign-up
+function venue_sign_up(){
+    let venue_data = {
+        venue_name:document.getElementById("venue_name").value,
+        password: document.getElementById("password").value,
+        phone: document.getElementById("phone").value,
+        location: document.getElementById("location").value
+    };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // app.get_post();
+        }
+    };
+    xhttp.open("POST","/venue_sign_up", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(venue_data));
+    console.log(venue_data);
+}
+
+//-----------------------------map--------------------------
+
+var app = new Vue({
+    el: '#mainpage',
+    data: {
+        markers:[]
+    },
+    methods:{
+        get_markers: function(){
+            let req = new XMLHttpRequest();
+            req.onreadystatechange = function(){
+                if(req.readyState == 4 && req.status == 200){
+                    app.markers = JSON.parse(req.responseText);
+                }
+            };
+            req.open('GET','/markers',true);
+            req.send();
+        }
+    }
+});
+
+app.get_markers();
+
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9jZWx5bjY2NiIsImEiOiJja29mZ2RwOWkwNTFvMnVwNzI3eXgxdngwIn0.IoVn3pEiBAmMgflGWs8eTw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v8',
+    center: [138.611, -34.923],
+    zoom: 9,
+    minZoom: 5,
+    maxZoom: 15
+});
+
+
+var geolocate = new mapboxgl.GeolocateControl({
+    // positionOptions: {
+    //     enableHighAccuracy: true
+    // },
+    trackUserLocation: true
+});
+
+// Add geolocate control to the map.
+map.addControl(geolocate);
+
+
+function storePosition(x, y, array) {
+    array.push(x);
+    array.push(y);
+}
+var x;
+var y;
+// var p = [];
+geolocate.on('geolocate',function(myLocation) {
+    x = myLocation.coords.longitude;
+    y = myLocation.coords.latitude;
+    // var position = [x, y];
+    // console.log(position);
+
+    var position = [];
+    storePosition(x, y, position);
+    // storePosition(19, 1000, position);
+    // storePosition(-300, 4578, position);
+
+    // position[0] == 3 ;  // x value (even indexes)
+    // position[1] == 5 ;  // y value (odd indexes)
+    // p[0] = pisition[0];
+    // p[1] = pisition[1];
+    console.log(position);//----------array to store user position
+});
+
+// console.log(p);
+
+
+// to loop through coordinate values
+// for (var i = 0; i < coords.length; i+=2) {
+//     var x = coords[i];
+//     var y = coords[i+1];
+// }
+
+
+
+
+//hotspot
+
+var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+});
+
+var coordinate = {};
+var coordinates = [];
+
+geocoder.on('result', function(e) {
+    // console.log(e.result);
+    var lat = e.result.center[0];
+    var long = e.result.center[1];
+    var cordName = e.result.text;
+    coordinate = {'lat':lat, 'long':long, 'location:':cordName};
+    coordinates.push(coordinate);
+    console.log(coordinate);
+    // console.log(coordinate);
+});
+
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+
+map.on('load', function() {
+
+    // coordinates = {[138.611, -34.923], []}
+    coordinates = [{'lat' : 138.611, 'long': -34.923},
+                        {'lat' : 138.5, 'long': -34.923},
+                        {'lat' : 114.16936109999999, 'long': 22.3193039}];
+
+    var features = [];
+    for (i = 0; i < coordinates.length; i++) {
+        data = {
+            'type': 'Feature',
+            "geometry": {
+                "type": "Point",
+                "coordinates": [coordinates[i].lat, coordinates[i].long]
+            }
+        };
+    features.push(data);
+    }
+
+    map.addSource("source_circle_500", {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": features
+        }
+    });
+
+    map.addLayer({
+        "id": "circle500",
+        "type": "circle",
+        "source": "source_circle_500",
+        "paint": {
+            "circle-radius": {
+                stops: [
+                    [5, 1],
+                    [15, 1024],
+                ],
+                base: 2
+            },
+        "circle-color": "red",
+        "circle-opacity": 0.3
+        }
+    });
+
+});
---
+++ projectAAA/routes/users.js
@@ -0,0 +1,277 @@
+var express = require('express');
+var router = express.Router();
+
+var log_in_user_id;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// let users = {
+//     test: 'password',
+//     test2: 'p2'
+// };
+
+
+//--------------------------------------user login--------------------------------------
+
+router.post('/login', function(req, res, next) {
+
+    user_id = req.body.user_id;
+    password = req.body.password;
+
+
+    req.pool.getConnection( function(err, connection){
+        //console.log('hello1');
+      if (err) {
+          res.sendStatus(500);
+          return;
+      }
+       //console.log('hello2');
+       var query = "SELECT user_id, password From user WHERE user_id = ? AND password =?";
+       params = [user_id, password];
+        if(user_id && password){
+           connection.query(query, params, function(err, rows, fields){
+               console.log(rows.length);
+            if(rows.length == 0){
+                res.sendStatus(500);
+                return;
+
+            }else{
+                req.session.loggedin = true;
+                req.session.user_id = user_id;
+                console.log("user : log in sucess.");
+                                log_in_user_id = user_id;
+                                console.log("user id", log_in_user_id);
+                res.send(user_id);
+            }
+
+
+            res.end();
+           });
+        }else{
+            res.send('Please enter Username and Password!');
+            res.end();
+        }
+
+    });
+
+
+});
+
+
+//--------------------------------------venue_log_in----------------------------------
+
+router.post('/login_v', function(req, res, next) {
+    venue_id = req.body.user_id;
+    password = req.body.password;
+
+
+    req.pool.getConnection( function(err, connection){
+        // console.log('hi1');
+      if (err) {
+          res.sendStatus(500);
+          return;
+      }
+    //   console.log('hi2');
+       var query = "SELECT venue_id, password FROM venue WHERE venue_id = ? AND password =?";
+       params = [venue_id, password];
+
+
+        if(venue_id && password){
+           connection.query(query, params, function(err, rows, fields){
+            //   console.log("params",params);
+            if(rows.length == 0){
+                res.sendStatus(500);
+                return;
+            }else{
+                req.session.loggedin = true;
+                req.session.venue_id = venue_id;
+                console.log("venue : log in sucess.");
+                log_in_user_id = venue_id;
+                console.log("venue id", log_in_user_id);
+                res.send(venue_id);
+            }
+
+            res.end();
+           });
+        }else{
+            res.send('Please enter Username and Password!');
+            res.end();
+        }
+
+    });
+});
+
+
+//------------------------------------------------official log in
+
+router.post('/login_o', function(req, res, next) {
+    official_id = req.body.user_id;
+    password = req.body.password;
+    req.pool.getConnection( function(err, connection){
+        //console.log('hello1');
+      if (err) {
+          res.sendStatus(500);
+          return;
+      }
+       //console.log('hello2');
+       var query = "SELECT official_id, password From health_official WHERE official_id = ? AND password =?";
+       params = [official_id, password];
+
+        if(official_id && password){
+           connection.query(query, params, function(err, rows, fields){
+            if(rows.length == 0){
+                res.sendStatus(500);
+                return;
+
+            }else{
+                req.session.loggedin = true;
+                req.session.user_id = official_id;
+                console.log("official : log in sucess.");
+                log_in_user_id = official_id;
+                res.send(official_id);
+            }
+
+            res.end();
+           });
+        }else{
+            res.send('Please enter Username and Password!');
+            res.end();
+        }
+
+    });
+});
+
+
+router.get('/take_user_id',function(req, res, next) {
+
+    res.send(log_in_user_id);
+    console.log("take user id ", log_in_user_id);
+    res.end();
+
+});
+
+
+
+// ------------------------------------individual user_info------------------------------
+router.get('/user_info', function(req, res, next) {
+
+    // if('user' in req.session){
+    //     console.log(req.session.user);
+    // }
+
+    req.pool.getConnection( function(err, connection){
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+
+        query = "SELECT user_id, last_name, first_name, phone, email FROM user WHERE user_id= ?";
+
+        params = [log_in_user_id];
+        console.log("userid",log_in_user_id);
+        // console.log(params);
+
+        connection.query(query, params, function(err, rows, fields){
+            connection.release();
+            if(err){
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+        });
+    });
+});
+/*
+//---------------------------------------------venue info -------------------------------------------
+
+router.get('/user_info', function(req, res, next) {
+
+    // if('user' in req.session){
+    //     console.log(req.session.user);
+    // }
+
+    req.pool.getConnection( function(err, connection){
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+
+
+        var query = "SELECT venue_id, venue_name, venue_location, contact_num FROM venue WHERE venue_id = ?";
+        params = [log_in_user_id];
+        console.log("venueid",log_in_user_id);
+        // console.log(params);
+
+        connection.query(query, params, function(err, rows, fields){
+            connection.release();
+            if(err){
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+            // console.log("hello");
+            // console.log(rows);
+        });
+    });
+});
+*/
+//------------------------------------------official info----------------------------------
+/*
+router.get('/user_info', function(req, res, next) {
+
+    // if('user' in req.session){
+    //     console.log(req.session.user);
+    // }
+
+    req.pool.getConnection( function(err, connection){
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+
+
+        var query = "SELECT official_id FROM health_official WHERE official_id = ?";
+        params = [log_in_user_id];
+        console.log("official id",log_in_user_id);
+        // console.log(params);
+
+        connection.query(query, params, function(err, rows, fields){
+            connection.release();
+            if(err){
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+            // console.log("hello");
+            // console.log(rows);
+        });
+    });
+});
+*/
+
+router.post('/logout', function(req, res, next) {
+    delete req.session.user;
+    res.send();
+                    log_in_user_id = 0;
+});
+
+//above here
+router.use(function(req, res, next){
+    //if user has loged in
+    if('user' in req.session){
+      next();
+    }else{
+        res.sendStatus(500);
+        res.sendStatus(401);
+    }
+});
+
+//after log in
+
+
+
+
+module.exports = router;
